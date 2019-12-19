4c9f5fa86b9f8d6329bfc96cde857165fe87215b
unknown <worl83@hunet-in.hunet.co.kr>
2019-12-19 21:45:07 +0900

    Hello JPA

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,53 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf =  Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx =  em.getTransaction();
+        tx.begin();
+
+        try
+        {
+            /* Member member = new Member();
+            member.setId(2L);
+            member.setName("helloB");
+            em.persist(member);*/
+
+
+
+            /*Member findMember = em.find(Member.class, 1L);*/
+            /*System.out.println("findMember.id = " +  findMember.getId());
+            System.out.println("findMember.name = " +  findMember.getName());*/
+
+            /*findMember.setName("HelloJPA");*/
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member : result)
+            {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+
+        } catch (Exception e)
+        {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
